161ed78395faf83ee763b93a004a9a478671072a
natasha-cloud <tindynatasha@gmail.com>
2023-10-29 20:43:39 +0300

    Added the changes in the settings.py and menu_list.html  to the project setup

--- restaurant_menu_app/forms.py
+++ restaurant_menu_app/forms.py
@@ -3,11 +3,12 @@ from django.forms import FileInput
 from .models import MenuItem
 
 class MenuItemForm(forms.ModelForm):
+    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={'accept': 'image/*'}))
+    
     class Meta:
         model = MenuItem
         fields = ['name', 'image', 'description', 'price', 'category']
 
         widgets = {
-            'image' : FileInput(attrs={'style': 'z-index:-1; position: absolute;', 'onchange': "changeTick('id_frontimg' ,'frontimg', 'frontimgicon', 'frontimg_error')"}),
-           
+            'image': FileInput(attrs={'style': 'z-index:-1; position: absolute;', 'onchange': "changeTick('id_frontimg' ,'frontimg', 'frontimgicon', 'frontimg_error')"}),
         }
--- restaurant_menu_project/settings.py
+++ restaurant_menu_project/settings.py
@@ -68,10 +68,9 @@ WSGI_APPLICATION = 'restaurant_menu_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -107,7 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'restaurant_menu_app/static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
